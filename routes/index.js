ebb852928daa20ae76f948a6ddc8bb6dcd753453
MINT <mint@inslens.com>
2018-12-29 15:40:54 +0800

    minify

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'DIMLAB - 数据实验室' });
+  res.render('index', { title: 'DIMLAB | 数据实验室' });
 });
 
 module.exports = router;
