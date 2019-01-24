58751f26599cb21d511cd0932a4d6b05d26db95d
MINT <mint@inslens.com>
2019-01-24 21:16:56 +0800

    navbar ready

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'DIMLAB | 数据实验室' });
+router.get("/", (req, res) => {
+  res.render("index", { title: "DIMLAB | 数据实验室" });
 });
 
 module.exports = router;
